c434de1a3326dcb6752c001f38d39873a3c9c61a
n3a209-Leon <n3a209@gmail.com>
2026-07-18 17:43:11 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,49 @@
+var CACHE_VERSION = 'wilds-v8';
+
+var ASSETS = [
+  './index.html',
+  './style.css',
+  './manifest.json',
+  './js/config.js',
+  './js/rng.js',
+  './js/world.js',
+  './js/inventory.js',
+  './js/resources.js',
+  './js/minimap.js',
+  './js/save.js',
+  './js/stats.js',
+  './js/mobs.js',
+  './js/build.js',
+  './js/craft.js',
+  './js/time.js',
+  './js/firebase-config.js',
+  './js/cloud.js',
+  './js/camera.js',
+  './js/input.js',
+  './js/player.js',
+  './js/render.js',
+  './js/main.js'
+];
+
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_VERSION).then(function(c) { return c.addAll(ASSETS); })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.map(function(k) {
+        if (k !== CACHE_VERSION) return caches.delete(k);
+      }));
+    }).then(function() { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).catch(function() { return caches.match(e.request); })
+  );
+});
