fd0c57fb01781f8c24cc1c96b96e4f5b70941210
n3a209-Leon <n3a209@gmail.com>
2026-07-19 18:58:46 +0800

    Update craft.js

--- js/craft.js
+++ js/craft.js
@@ -4,7 +4,7 @@ window.W = window.W || {};
    新增欄位時務必同步 save.js 的 collect / apply / migrate 三處。 */
 W.Craft = (function() {
 
-  var gear = { axe: false, pick: false, bow: false };
+  var gear = { axe: false, pick: false, bow: false, maxe: false, mpick: false };
 
   /* kind: tool = 取得裝備；place = 放置建造物；item = 產出物品
      need: 需要站在某種建造物旁（null 代表隨處可做） */
@@ -16,6 +16,11 @@ W.Craft = (function() {
     { id: 'fire', name: '\u71df\u706b',   icon: '\uD83D\uDD25', kind: 'place', place: 0, cost: { wood: 5, stone: 3 }, desc: '\u65c1\u908a\u53ef\u4ee5\u70e4\u8089' },
     { id: 'wall', name: '\u6728\u7246',   icon: '\uD83E\uDDF1', kind: 'place', place: 1, cost: { wood: 4 }, desc: '\u963b\u64cb\u72fc\u7fa4' },
     { id: 'bed',  name: '\u7761\u888b',   icon: '\uD83D\uDECF\uFE0F', kind: 'place', place: 2, cost: { fiber: 8, hide: 3 }, desc: '\u91cd\u65b0\u8a2d\u5b9a\u71df\u5730' },
+    { id: 'furnace', name: '\u7194\u7210', icon: '\uD83C\uDFED', kind: 'place', place: 3, cost: { stone: 12, wood: 4 }, desc: '\u65c1\u908a\u53ef\u4ee5\u7194\u7149\u91d1\u5c6c' },
+    { id: 'metal', name: '\u7194\u7149\u91d1\u5c6c', icon: '\uD83D\uDD29', kind: 'item', give: { metal: 1 }, cost: { stone: 4, flint: 2, wood: 2 }, need: 3, desc: '\u9700\u8981\u7ad9\u5728\u7194\u7210\u65c1' },
+    { id: 'maxe',  name: '\u91d1\u5c6c\u65a7', icon: '\uD83E\uDE93', kind: 'tool', cost: { metal: 2, wood: 2 }, needGear: 'axe', desc: '\u4f10\u6728\uff0b4\uff0c\u653b\u64ca\uff0b14\uff08\u9700\u5148\u6709\u77f3\u65a7\uff09' },
+    { id: 'mpick', name: '\u91d1\u5c6c\u93ac', icon: '\u26CF\uFE0F', kind: 'tool', cost: { metal: 2, wood: 2 }, needGear: 'pick', desc: '\u63a1\u77f3\uff0b4\uff08\u9700\u5148\u6709\u77f3\u93ac\uff09' },
+    { id: 'soup',  name: '\u8611\u83c7\u6e6f', icon: '\uD83C\uDF72', kind: 'item', give: { soup: 1 }, cost: { mushroom: 2, berry: 1 }, need: 0, desc: '\u9700\u8981\u71df\u706b\uff1b\u5403\u4e86\u5927\u5e45\u56de\u5fa9' },
     { id: 'cook', name: '\u70e4\u8089',   icon: '\uD83C\uDF57', kind: 'item',  give: { cooked: 1 }, cost: { meat: 1 }, need: 0, desc: '\u9700\u8981\u7ad9\u5728\u71df\u706b\u65c1' }
   ];
 
@@ -68,6 +73,7 @@ W.Craft = (function() {
 
     if (r.kind === 'tool') {
       if (gear[r.id]) return '\u5df2\u7d93\u64c1\u6709\u4e86';
+      if (r.needGear && !gear[r.needGear]) return '\u9700\u8981\u5148\u88fd\u4f5c\u524d\u7f6e\u5de5\u5177';
       pay(r);
       gear[r.id] = true;
       return true;
@@ -102,28 +108,41 @@ W.Craft = (function() {
 
   function has(id) { return !!gear[id]; }
 
-  function attackBonus() { return gear.axe ? W.CFG.AXE_ATK_BONUS : 0; }
+  function attackBonus() {
+    if (gear.maxe) return W.CFG.METAL_ATK_BONUS;
+    return gear.axe ? W.CFG.AXE_ATK_BONUS : 0;
+  }
 
   function yieldBonus(resType) {
-    if (resType === 0 && gear.axe) return W.CFG.TOOL_YIELD_BONUS;
-    if (resType === 1 && gear.pick) return W.CFG.TOOL_YIELD_BONUS;
+    if (resType === 0) {
+      if (gear.maxe) return W.CFG.METAL_YIELD_BONUS;
+      if (gear.axe) return W.CFG.TOOL_YIELD_BONUS;
+    }
+    if (resType === 1) {
+      if (gear.mpick) return W.CFG.METAL_YIELD_BONUS;
+      if (gear.pick) return W.CFG.TOOL_YIELD_BONUS;
+    }
     return 0;
   }
 
   function exportData() {
-    return { axe: !!gear.axe, pick: !!gear.pick, bow: !!gear.bow };
+    return { axe: !!gear.axe, pick: !!gear.pick, bow: !!gear.bow, maxe: !!gear.maxe, mpick: !!gear.mpick };
   }
 
   function importData(o) {
     gear.axe = !!(o && o.axe);
     gear.pick = !!(o && o.pick);
     gear.bow = !!(o && o.bow);
+    gear.maxe = !!(o && o.maxe);
+    gear.mpick = !!(o && o.mpick);
   }
 
   function clear() {
     gear.axe = false;
     gear.pick = false;
     gear.bow = false;
+    gear.maxe = false;
+    gear.mpick = false;
   }
 
   return {
